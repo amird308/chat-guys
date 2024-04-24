dcfb62a9f37cce2ddfaedc0903a8b0ae499c09cf
damirchi <damirchi@pro.ai>
2024-04-24 15:04:32 -0700

    feat: init frontend project

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/frontend',
+  cacheDir: '../../node_modules/.vite/app/frontend',
 
   server: {
     port: 4200,
@@ -25,25 +25,10 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../dist/frontend',
+    outDir: '../../dist/app/frontend',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
-    },
-  },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: '../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../coverage/frontend',
-      provider: 'v8',
     },
   },
 });
